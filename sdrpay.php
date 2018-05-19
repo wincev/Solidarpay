de4ed218efc832390b4aea82f6cfdc0d35ece197
wincev <info@winc-ev.org>
2018-05-19 07:48:10 +0200

    Complete rework
    remove unneeded code and restructured saving payment data.

--- sdrpay.php
+++ sdrpay.php
@@ -1,22 +1,33 @@
-<?php 
-/** 
+ <?php
+/**
 * Plugin Name: SolidarPay
 * Plugin URI: https://solidar.it
-* Description: A plugin to split payments between Solidar bot and fiat currencies via PayPal.
+* Description: A plugin to split payments between Solidar bot and fiat currencies
 * Author: Hendrik Richter
 * Version: 1.0
 * License: GPLv2
 */
 
+//require_once 'sdr-wc-cart.php';
+
 // Display Fields
 add_action('woocommerce_product_options_general_product_data', 'solidar_sdrprice_field');
-
-// Save Fields
-add_action('woocommerce_process_product_meta', 'solidar_sdrprice_field_save');
-
 function solidar_sdrprice_field() {
   global $woocommerce, $post;
   echo '<div class="product_custom_field">';
+  //Merchant field
+  woocommerce_wp_text_input(
+    array(
+      'id' => '_sdrmerchant_field',
+      'placeholder' => 'Solidar Merchant Field',
+      'label' => __('t.me/SolidarMerchant_bot ID:'),
+      'type' => 'number',
+      'custom_attributes' => array(
+        'step' => 'any',
+      ),
+    )
+  );
+  echo '</div>';
   //Solidar Price Field
   woocommerce_wp_text_input(
     array(
@@ -33,18 +44,22 @@ function solidar_sdrprice_field() {
   echo '</div>';
 }
 
+// Save Fields
+add_action('woocommerce_process_product_meta', 'solidar_sdrprice_field_save');
 function solidar_sdrprice_field_save($post_id) {
   // Custom Product Number Field
   $woocommerce_sdrprice_field = $_POST['_sdrprice_field'];
-  if (!empty($woocommerce_sdrprice_field))
+  $woocommerce_sdrmerchant_field = $_POST['_sdrmerchant_field'];
+  if (!empty($woocommerce_sdrmerchant_field)) {
     update_post_meta($post_id, '_sdrprice_field', esc_attr($woocommerce_sdrprice_field));
+    update_post_meta($post_id, '_sdrmerchant_field', esc_attr($woocommerce_sdrmerchant_field));
+  }
 }
 
 // add solidar price on product page
 add_action('woocommerce_get_price_html', 'solidar_show_sdrprice');
 // add solidar price on shop overview page
 add_action('woocommerce_after_shop_loop_item', 'solidar_show_sdrprice', 9 );
-
 function solidar_show_sdrprice( $price ) {
   $price_field = get_post_meta(get_the_ID(), '_sdrprice_field', true);
   if (empty($price_field)) {
@@ -52,95 +67,6 @@ function solidar_show_sdrprice( $price ) {
   }
   return $price .  "<span> <br/> Solidar: $price_field";
 }
-
-// Make sure WooCommerce is active
-
-add_action('plugins_loaded', 'solidar_pay_gateway_init' );
-
-function solidar_pay_gateway_init() {
-    //if condition use to do nothin while WooCommerce is not installed
-	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
-	class solidar_pay_Gateway extends WC_Payment_Gateway {
-	function __construct() {
-		$this->id = "solidar_pay";
-		$this->method_title = __( "Solidar split payment", 'solidar-pay-gateway' );
-		$this->method_description = __( "Soliar split payments", 'solidar-pay-gateway' );
-		$this->title = __( "Solidar split payment", 'solidar-pay-gateway' );
-		$this->icon = null;
-		$this->has_fields = true;
-		$this->supports = array( 'products'); //default_credit_card_form' );
-		$this->init_form_fields();
-		$this->init_settings();
-		foreach ( $this->settings as $setting_key => $value ) {
-			$this->$setting_key = $value;
-		}
-
-		add_action( 'admin_notices', array( $this,	'do_ssl_check' ) );
-		if ( is_admin() ) {
-			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		}
-	} // Here is the  End __construct()
-
-	// administration fields for specific Gateway
-	public function init_form_fields() {
-		$this->form_fields = array(
-			'enabled' => array(
-				'title'		=> __( 'Enable / Disable', 'cwoa-authorizenet-aim' ),
-				'label'		=> __( 'Enable this payment gateway', 'cwoa-authorizenet-aim' ),
-				'type'		=> 'checkbox',
-				'default'	=> 'no',
-			),
-			'title' => array(
-				'title'		=> __( 'Title', 'cwoa-authorizenet-aim' ),
-				'type'		=> 'text',
-				'desc_tip'	=> __( 'Payment title of checkout process.', 'cwoa-authorizenet-aim' ),
-				'default'	=> __( 'Solidar split payment', 'cwoa-authorizenet-aim' ),
-			),
-			'description' => array(
-				'title'		=> __( 'Description', 'cwoa-authorizenet-aim' ),
-				'type'		=> 'textarea',
-				'desc_tip'	=> __( 'Payment title of checkout process.', 'cwoa-authorizenet-aim' ),
-				'default'	=> __( 'Solidar payment received.', 'cwoa-authorizenet-aim' ),
-				'css'		=> 'max-width:450px;'
-			),
-			'merchant_id' => array(
-				'title'		=> __( 'Telegram merchant ID', 'cwoa-authorizenet-aim' ),
-				'type'		=> 'text',
-				'desc_tip'	=> __( 'Please start a group chat with our Telegram Solidar_Merchant_bot to open a merchant account.', 'cwoa-authorizenet-aim' ),
-			),
-                        'solidar_payed_url' => array(
-                                'title'         => __( 'Bot URL to send payment confirmations to.', 'cwoa-authorizenet-aim' ),
-                                'type'          => 'text',
-                                'desc_tip'      => __( 'URL where the Bot will send a confirmation to when the amount is payed.', 'cwoa-authorizenet-aim' ),
-                        ),
-
-			'environment' => array(
-				'title'		=> __( 'Authorize.net Test Mode', 'cwoa-authorizenet-aim' ),
-				'label'		=> __( 'Enable Test Mode', 'cwoa-authorizenet-aim' ),
-				'type'		=> 'checkbox',
-				'description' => __( 'This is the test mode of gateway.', 'cwoa-authorizenet-aim' ),
-				'default'	=> 'no',
-			)
-		);
-	}
-	}
-	//include_once( 'solidar-gateway-class.php' );
-	// class add it too WooCommerce
-	add_filter( 'woocommerce_payment_gateways', 'solidar_add_payment_gateway' );
-	function solidar_add_payment_gateway( $methods ) {
-		$methods[] = 'solidar_pay_Gateway';
-		return $methods;
-	}
-}
-// Add custom action links
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'cwoa_authorizenet_aim_action_links' );
-function cwoa_authorizenet_aim_action_links( $links ) {
-	$plugin_links = array(
-		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout' ) . '">' . __( 'Settings', 'cwoa-authorizenet-aim' ) . '</a>',
-	);
-	return array_merge( $plugin_links, $links );
-}
-
 
 //add total product price in cart
 add_filter('woocommerce_get_item_data', 'display_cart_item_solidar_price', 11, 2);
@@ -152,61 +78,62 @@ function display_cart_item_solidar_price($item_data, $cart_item) {
   $solidar_price = get_post_meta($item_id, '_sdrprice_field', true);
   $solidar_display = $solidar_price * $product_qty;
   $solidar_total = $solidar_price * $product_qty;
-
   $item_data[] = array(
-    'key'     => __('SDR', 'woocommerce'),
+    'key'     => __('SDR'),
     'value'   => $solidar_price,
     'display' => $solidar_display,
   );
-
   return $item_data;
 }
 
-// add subtotal Solidar amount to cart
+// add subtotal Solidar amount per merchant to cart
 add_filter('woocommerce_cart_subtotal', 'add_solidar_subtotal');
 function add_solidar_subtotal($product_subtotal) {
   $solidar_total = 0;
-  foreach( WC()->cart->get_cart() as $cart_item ){
-    $product_id = $cart_item['product_id'];
-    $product_qty = $cart_item['quantity'];
-    $solidar_price = get_post_meta($product_id, '_sdrprice_field', true);
-    $solidar_total = $solidar_total + $solidar_price * $product_qty;
-  }
-  if($solidar_total == 0)   return $product_subtotal;
-
-  return $product_subtotal . '<br/>SDR: ' . $solidar_total;
-}
-
-//check how much solidar each vendor gets paid and request payment id
-add_action('woocommerce_checkout_create_order', 'add_solidar_payment_id', 20, 2);
-function add_solidar_payment_id( $order, $data ) {
   $solidar_order = array();
+
   foreach( WC()->cart->get_cart() as $cart_item ){
     $solidar_total = 0;
     $product_id = $cart_item['product_id'];
     $product_qty = $cart_item['quantity'];
     $solidar_price = get_post_meta($product_id, '_sdrprice_field', true);
     $solidar_total = $solidar_total + $solidar_price * $product_qty;
+    $solidar_per_item = $solidar_price * $product_qty;
     $solidar_merchant = get_post_meta($product_id, '_sdrmerchant_field', true);
-    if($solidar_order[$solidar_merchant] == 0 ) {
-      $solidar_order[$solidar_merchant] = $solidar_price;
+    if( empty($solidar_order['solidarpay']['merchant'][$solidar_merchant]) ) {
+      $solidar_order['solidarpay']['merchant'][$solidar_merchant] = $solidar_per_item;
     }
     else {
-    $solidar_order[$solidar_merchant] = $solidar_order[$solidar_merchant] + $solidar_price;
+    $solidar_order['solidarpay']['merchant'][$solidar_merchant] = $solidar_order['solidarpay']['merchant'][$solidar_merchant] + $solidar_per_item;
     }
   }
-  if (empty($solidar_order)) return;
-  //build the payment request:
-  $requestURL = 'https://solidar.it/merchant/generatePaymentID.php?';
-  $order->update_meta_data('_solidar_payment_id', $payment_id);
-  $order->update_meta_data('_solidar_payment_confirm', 'no');
+
+  if($solidar_total == 0)   return $product_subtotal;
+
+  WC()->cart->set_removed_cart_contents($solidar_order);
+  return $product_subtotal . '<br/>SDR: ' . $solidar_total;
 }
 
+//Deny place order until solidar amount is payed
+add_action( 'woocommerce_order_button_html', 'filter_woocommerce_order_button_html', 10, 1 );
+function filter_woocommerce_order_button_html( $button_place_order ) {
+  $requestURL = 'https://solidar.it/merchant/generatePaymentID.php?';
 
-//remove place order until solidar amount is payed
-add_filter( 'woocommerce_order_button_html', 'filter_woocommerce_order_button_html', 10, 1 );
-function filter_woocommerce_order_button_html( $input_type_submit_class_button_alt_name_woocommerce_chec$
-    // make filter magic happen here...
-    return; // $input_type_submit_class_button_alt_name_woocommerce_checkout_place_order_id_place_order_$
-};
+  $reload_checkout = get_permalink( wc_get_page_id( 'checkout' ) );
+  echo '<a href="' . $reload_checkout . '">Extra Button</a>';
+  $pay_array = WC()->cart->get_removed_cart_contents();
+  foreach($pay_array as $key => $content){
+    echo "Key: $key Content: $content </br>" ;
+  }
 
+  $removed_contents =  WC()->cart->get_removed_cart_contents();
+  $solidarpay = $removed_contents['solidarpay'];
+  if (empty($solidarpay['pay_id']) && empty($solidarpay['merchant'])) {
+    return $button_place_order;
+  }
+  echo 'To continue please settle the m.me/solidar.winc payment first:';
+
+  return;
+}
+
+?>
